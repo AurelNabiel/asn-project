9d58cbfd8f43d459a94c7494dfd16b65159d19dd
ROFL1ST <danendrapr55@gmail.com>
2024-09-17 22:44:29 +0700

    Fix: navbar active classname

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,29 +1,34 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import LogoKemendagri from "../assets/LogoKemendagri.png";
-import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const location = useLocation(); 
+
+
+  const isActive = (path) => location.pathname === path ? "text-[#D49F1B]" : "hover:text-[#D49F1B]";
+
   return (
     <>
-      <div class="absolute inset-x-0 top-0 mt-10 w-3/4 rounded-full container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center bg-[#063A69]">
-        <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center text-white font-semibold ">
+      <div className="absolute inset-x-0 top-0 mt-10 w-3/4 rounded-full container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center bg-[#063A69]">
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center text-white font-semibold">
           <Link to={"/"}>
-            <p class="mr-10 hover:text-[#D49F1B]">Beranda</p>
+            <p className={`mr-10 ${isActive("/")}`}>Beranda</p>
           </Link>
           <Link to={"/pengumuman"}>
-            <p class="mr-10 hover:text-[#D49F1B]">Pengumuman</p>
+            <p className={`mr-10 ${isActive("/pengumuman")}`}>Pengumuman</p>
           </Link>
           <Link to={"/konsultasi"}>
-            <p class="mr-10 hover:text-[#D49F1B]">Konsultasi</p>
+            <p className={`mr-10 ${isActive("/konsultasi")}`}>Konsultasi</p>
           </Link>
           <Link to={"/pengacara"}>
-            <p class="mr-10 hover:text-[#D49F1B]">Pengacara</p>
+            <p className={`mr-10 ${isActive("/pengacara")}`}>Pengacara</p>
           </Link>
           <Link to={"/pengadaan"}>
-            <p class="mr-10 hover:text-[#D49F1B]">Pengadaan</p>
+            <p className={`mr-10 ${isActive("/pengadaan")}`}>Pengadaan</p>
           </Link>
           <Link to={"/pesan"}>
-            <p class=" hover:text-[#D49F1B]">Kotak pesan</p>
+            <p className={`${isActive("/pesan")}`}>Kotak pesan</p>
           </Link>
         </nav>
       </div>
